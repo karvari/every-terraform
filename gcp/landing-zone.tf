96f03900a48547c16c9db3ec4967c617fb6319f4
Karvari Ellingson <mzphitt@gmail.com>
2023-11-14 13:52:48 -0800

    Update landing-zone.tf

--- gcp/landing-zone.tf
+++ gcp/landing-zone.tf
@@ -1,5 +1,24 @@
-# resources include folder structure, network, super users, and initial projects
+# resources include folder structure, network, super users, terraform code storage, and a shared resources project
 
+resource "random_id" "bucket_prefix" {
+  byte_length = 8
+}
+
+resource "google_storage_bucket" "default" {
+  name          = "${random_id.bucket_prefix.hex}-bucket-tfstate"
+  force_destroy = false
+  location      = "US"
+  storage_class = "STANDARD"
+  versioning {
+    enabled = true
+  }
+  encryption {
+    default_kms_key_name = google_kms_crypto_key.terraform_state_bucket.id
+  }
+  depends_on = [
+    google_project_iam_member.default
+  ]
+}
 resource "google_compute_network" "vpc_network" {
   name                    = "shared-vpc"
   auto_create_subnetworks = false
