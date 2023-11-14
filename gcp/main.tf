373f4d9b4fb175d1791d0077c15e0312ccd9c6b5
Karvari Ellingson <mzphitt@gmail.com>
2023-11-14 14:33:50 -0800

    Update main.tf

--- gcp/main.tf
+++ gcp/main.tf
@@ -34,6 +34,13 @@ resource "google_compute_instance" "default" {
     initialize_params {
       image = "debian-cloud/debian-11"
     }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+}
   }
 
   # Install Flask
