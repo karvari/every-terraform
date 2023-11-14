ec5f8d49ec085d58d162bc72819431be6de6ee7d
Karvari Ellingson <mzphitt@gmail.com>
2023-11-14 13:50:31 -0800

    Update main.tf

--- gcp/main.tf
+++ gcp/main.tf
@@ -1,9 +1,54 @@
-data "google_access_approval_folder_service_account" "service_account" {
-  folder_id = "my-folder"
+package main
+
+resource "google_compute_firewall" "ssh" {
+  name = "allow-ssh"
+  allow {
+    ports    = ["22"]
+    protocol = "tcp"
+  }
+  direction     = "INGRESS"
+  network       = google_compute_network.vpc_network.id
+  priority      = 1000
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["ssh"]
 }
-data "google_access_approval_organization_service_account" "service_account" {
-  organization_id = "my-organization"
+resource "google_compute_firewall" "flask" {
+  name    = "flask-app-firewall"
+  network = google_compute_network.vpc_network.id
+
+  allow {
+    protocol = "tcp"
+    ports    = ["5000"]
+  }
+  source_ranges = ["0.0.0.0/0"]
 }
-data "google_access_approval_project_service_account" "service_account" {
-  project_id = "my-project"
+
+# Create a single Compute Engine instance
+resource "google_compute_instance" "default" {
+  name         = "flask-vm"
+  machine_type = "f1-micro"
+  zone         = "us-west1-a"
+  tags         = ["ssh"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  # Install Flask
+  metadata_startup_script = "sudo apt-get update; sudo apt-get install -yq build-essential python3-pip rsync; pip install flask"
+
+  network_interface {
+    subnetwork = google_compute_subnetwork.default.id
+
+    access_config {
+      # Include this section to give the VM an external IP address
+    }
+  }
+}
+
+// A variable for extracting the external IP address of the VM
+output "Web-server-URL" {
+ value = join("",["http://",google_compute_instance.default.network_interface.0.access_config.0.nat_ip,":5000"])
 }
